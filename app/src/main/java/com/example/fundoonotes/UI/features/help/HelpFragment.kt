8f1d09f0e75ee48e77065aaba1d8b332be6d1530
Maanas28 <maanaskanwar88@gmail.com>
2025-04-18 10:01:07 +0530

    Made some required changes.

--- app/src/main/java/com/example/fundoonotes/UI/features/help/HelpFragment.kt
+++ app/src/main/java/com/example/fundoonotes/UI/features/help/HelpFragment.kt
@@ -1,18 +1,36 @@
 package com.example.fundoonotes.UI.features.help
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.fundoonotes.R
+import com.example.fundoonotes.UI.components.SearchBarFragment
+import com.example.fundoonotes.UI.util.SearchListener
 
-class HelpFragment : Fragment() {
+class HelpFragment : Fragment(), SearchListener {
+
+    private lateinit var searchBar: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_help, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        searchBar = view.findViewById(R.id.searchBarContainerHelp)
+
+        val searchBarFragment = SearchBarFragment.newInstance("Help")
+        childFragmentManager.beginTransaction()
+            .replace(R.id.searchBarContainerHelp, searchBarFragment)
+            .commit()
+    }
+
+    override fun onSearchQueryChanged(query: String) {
+
     }
 }
